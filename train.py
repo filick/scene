0cfdc56751b1ce2390fe33d2e63564662e034eec
Yan Wang <wangyan14@mails.tsinghua.edu.cn>
2017-10-18 15:13:22 -0400

    add two torch model

--- train.py
+++ train.py
@@ -49,6 +49,7 @@ def run():
         if os.path.isfile(latest_check):
             print("=> loading checkpoint '{}'".format(latest_check))
             checkpoint = torch.load(latest_check)
+            global start_epoch 
             start_epoch = checkpoint['epoch']
             best_prec1 = checkpoint['best_prec1']
             model.load_state_dict(checkpoint['state_dict'])
